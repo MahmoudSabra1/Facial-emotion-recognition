67d613e851930beadf4567870fa8e86d3ca13280
Elsayed Akram <elsayed5454@gmail.com>
2020-10-14 20:30:36 +0200

    3 predict files tested

--- img_predict.py
+++ img_predict.py
@@ -1,27 +1,31 @@
 import cv2
+import argparse
 import numpy as np
 from keras.models import model_from_json
 from keras.preprocessing import image
 
-# Loading JSON model
+# Parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required=True, help='path to input image file')
+args = vars(ap.parse_args())
+
+# Load model from JSON file
 json_file = open('Saved-Models\\model8402.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
 
-# Loading weights
+# Load weights and them to model
 model.load_weights('Saved-Models\\model8402.h5')
-print('Model and weights are loaded and compiled.')
 
-face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-test_img = cv2.imread('pic')
-
-gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+img = cv2.imread(args['image'])
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces_detected = classifier.detectMultiScale(gray_img, 1.1, 5)
 
 for (x, y, w, h) in faces_detected:
-    cv2.rectangle(test_img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
     roi_gray = gray_img[y:y + w, x:x + h]
     roi_gray = cv2.resize(roi_gray, (48, 48))
     img_pixels = image.img_to_array(roi_gray)
@@ -34,9 +38,10 @@ for (x, y, w, h) in faces_detected:
     emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear']
     predicted_emotion = emotions[max_index]
 
-    cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
 
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ', resized_img)
+resized_img = cv2.resize(img, (1000, 700))
+cv2.imshow('Facial Emotion Recognition', resized_img)
 
-cv2.waitKey(0)
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
--- vid_predict.py
+++ vid_predict.py
@@ -1,8 +1,14 @@
+import argparse
 import cv2
 import numpy as np
 from imutils.video import FileVideoStream
 from keras.models import model_from_json
 from keras.preprocessing import image
+
+# Parse the video file path argument
+ap = argparse.ArgumentParser()
+ap.add_argument('-v', '--video', required=True, help='path to input video file')
+args = vars(ap.parse_args())
 
 # Loading JSON model
 json_file = open('Saved-Models\\model8402.json', 'r')
@@ -12,21 +18,20 @@ model = model_from_json(loaded_model_json)
 
 # Loading weights
 model.load_weights('Saved-Models\\model8402.h5')
-print('Model and weights are loaded and compiled.')
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-fvs = FileVideoStream('emotions2.mp4').start()
+fvs = FileVideoStream(args['video']).start()
 
 while fvs.more():
-    test_img = fvs.read()
+    img = fvs.read()
 
-    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.1, 6, minSize=(200, 200))
 
     for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
         roi_gray = gray_img[y:y + w, x:x + h]
         roi_gray = cv2.resize(roi_gray, (48, 48))
         img_pixels = image.img_to_array(roi_gray)
@@ -39,11 +44,13 @@ while fvs.more():
         emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear']
         predicted_emotion = emotions[max_index]
 
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
 
-        resized_img = cv2.resize(test_img, (1000, 700))
-        cv2.imshow('Facial emotion analysis ', resized_img)
-        cv2.waitKey(1)
+        resized_img = cv2.resize(img, (1000, 700))
+        cv2.imshow('Facial Emotion Recognition', resized_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 fvs.stop()
 cv2.destroyAllWindows()
