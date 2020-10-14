0ec2b809f40f2ef91cf16328cb0dd47c3597d39a
Elsayed Akram <elsayed5454@gmail.com>
2020-10-14 19:08:43 +0200

    Testing live_cam_predict.py

--- live_cam_predict.py
+++ live_cam_predict.py
@@ -3,30 +3,29 @@ import numpy as np
 from keras.models import model_from_json
 from keras.preprocessing import image
 
-# Loading JSON model
+# Load model from JSON file
 json_file = open('Saved-Models\\model8402.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 model = model_from_json(loaded_model_json)
 
-# Loading weights
+# Load weights and them to model
 model.load_weights('Saved-Models\\model8402.h5')
-print('Model and weights are loaded and compiled.')
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-stream = cv2.VideoCapture('emotions2.mp4')
+cap = cv2.VideoCapture(0)
 
 while True:
-    ret, test_img = stream.read()
+    ret, img = cap.read()
     if not ret:
         break
 
-    gray_img = cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
-    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.1, 6)
 
     for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
         roi_gray = gray_img[y:y + w, x:x + h]
         roi_gray = cv2.resize(roi_gray, (48, 48))
         img_pixels = image.img_to_array(roi_gray)
@@ -39,13 +38,13 @@ while True:
         emotions = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear']
         predicted_emotion = emotions[max_index]
 
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (255, 255, 255), 2)
 
-        resized_img = cv2.resize(test_img, (1000, 700))
-        cv2.imshow('Facial emotion analysis ', resized_img)
+        resized_img = cv2.resize(img, (1000, 700))
+        cv2.imshow('Facial Emotion Recognition', resized_img)
 
-        if cv2.waitKey(10) == ord('q'):  # wait until 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-stream.release()
+cap.release()
 cv2.destroyAllWindows()
